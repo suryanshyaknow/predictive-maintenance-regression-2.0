5710beca165ae8db7af4e6ce190d3aed94732f2f
Suryansh <suryanshgrover1999@gmail.com>
2022-10-26 19:16:45 +0530

    stage2: split data

---
+++ src/split_data.py
@@ -0,0 +1,47 @@
+# Gist: Splitting the data into training and test subsets and storing in the data/processed dir.
+
+import pandas as pd
+import argparse
+from sklearn.model_selection import train_test_split
+from read_params import read_params
+
+
+def split_and_save(cfile_path):  # cfile_path : Configuration File path.
+
+    # fetching the relevants
+    config = read_params(cfile_path)
+    data_path = config["load_data"]["prepared"]
+    label = config["base"]["label"]
+    test_size = config["split_data"]["test_size"]
+    random_state = config["split_data"]["random_state"]
+    
+    # paths to save test and train data
+    training_features_path = config["split_data"]["training_features_path"]
+    training_labels_path = config["split_data"]["training_labels_path"]
+    test_features_path = config["split_data"]["test_features_path"]
+    test_labels_path = config["split_data"]["test_labels_path"]
+
+
+    df = pd.read_csv(data_path)
+
+    # features
+    X = df.drop(columns=(label))
+    Y = df[label]
+
+    # splitting into training and test subsets
+    x_train, x_test, y_train, y_test = train_test_split(
+        X, Y, test_size=test_size, random_state=random_state
+    )
+
+    # saving them to the data/processed directory
+    x_train.to_csv(training_features_path, index=None)
+    y_train.to_csv(training_labels_path, index=None)
+    x_test.to_csv(test_features_path, index=None)
+    y_test.to_csv(test_labels_path, index=None)
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parse_args = args.parse_args()
+    split_and_save(cfile_path=parse_args.config)
