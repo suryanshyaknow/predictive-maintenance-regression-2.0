b0cee2793b10ae6fef9154ea32271f46821d8eb2
Suryansh <suryanshgrover1999@gmail.com>
2022-11-01 19:29:41 +0530

    done with setup!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Predictive-Maintenance-Regression-2.0",
+    author="suryanshyaknow",
+    license="MIT",
+    packages=find_packages()
+)
